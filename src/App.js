3733b2ab9bf85eec7f396d8bda1df6b2c8f8e5f1
Anthony <aamay022@fiu.edu>
2021-10-08 16:15:19 -0400

    star wars characters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [searchBar ,setSearchBar] = useState('')
+const [characters, setCharacters] = useState([])
+
+useEffect(()=>{
+  axios.get('https://swapi.dev/api/people')
+  .then(res => {
+    console.log(res)
+    setCharacters(res.data)
+    console.log(characters)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+}, [])
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,9 +31,21 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+      {characters.map((char,idx) => {
+      return (
+      <div>
+        <Character data ={char}/>
+      </div>
+      )
+      })}
+      
     </div>
   );
 }
 
 export default App;
+
+{/* {characters.map(char,idx =>{ 
+        return <Character key={idx} info={char}/>
+      })} */}
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+const StyledBar = styled.div`
+display: flex;
+width: 100%;
+justify-content:center;
+margin-top: 20px;
+font-size: 3rem;
+
+.top{
+    display: flex;
+    justify-content: space-between;
+    width: 110%;
+    border: 2px solid #c2b280;
+}
+`
+
+export default function Character (props) {
+    const [displayOn, setDisplayOn] = useState(false)
+
+return (
+    <StyledBar>
+    <nav className = 'NavBarItems'>
+        <div className = 'top'><h1 className = 'navbar-logo'>{props.data.name}</h1>
+        <div className= 'fas fa-bars' onClick={()=> displayOn === false ? setDisplayOn(true) : setDisplayOn(false)}>
+        </div></div>
+        {displayOn && <ul>
+            <li>Gender: {props.data.gender}</li>
+            <li>Height: {props.data.height}</li>
+            <li>Mass: {props.data.mass}</li>
+            <li>Birth Year: {props.data.birth_year}</li>
+            <li>Eye Color: {props.data.eye_color}</li>
+            <li>Hair Color: {props.data.hair_color}</li>
+        </ul>}
+    </nav>
+    </StyledBar>
+)
+}
